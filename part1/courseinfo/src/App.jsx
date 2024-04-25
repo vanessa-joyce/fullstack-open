c8892b85a8db783a9480103a7ddc6630963d370c
Vanessa Sutter <vanessa.sutter@frontify.com>
2024-04-25 22:49:35 +0200

    exercises 1.1-1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,24 @@
+import Content from "./Content";
+import Header from "./Header";
+import Total from "./Total";
+
+const App = () => {
+  const course = "Half Stack application development";
+  const exercises1 = 10;
+  const exercises2 = 7;
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,6 @@
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  console.log(exercises1);
+  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+};
+
+export default Total;
