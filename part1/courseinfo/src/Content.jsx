1f54aaad830008a26817937fc52633449164e92a
Vanessa Sutter <vanessa@sutter.cloud>
2024-04-27 22:16:53 +0200

    exercise 1.3

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,18 +1,12 @@
 import Part from "./Part";
 
-const Content = () => {
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
+const Content = (props) => {
+  console.log(props.parts);
   return (
     <>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={props.parts.part1.name} exercises={props.parts.part1.exercises} />
+      <Part part={props.parts.part2.name} exercises={props.parts.part2.exercises} />
+      <Part part={props.parts.part3.name} exercises={props.parts.part3.exercises} />
     </>
   );
 };
