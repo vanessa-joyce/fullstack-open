58d53b32c3d667c3ca4804cd11cd80100ff32036
Vanessa Sutter <vanessa@sutter.cloud>
2024-10-16 11:02:50 +0200

    Complex state experiment

--- part1/counter/src/App.jsx
+++ part1/counter/src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import Display from './Display'
-import Button from './Button'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [clicks, setClicks] = useState({left: 0, right: 0})
+  
 
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-
-  }
-
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
+  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {clicks.left}
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      {clicks.right}
     </div>
   )
 }
