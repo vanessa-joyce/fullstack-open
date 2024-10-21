4f76f6ae17d90cbf6af1d011e89f56b6e30dfc99
Vanessa Sutter <vanessa@sutter.cloud>
2024-10-21 22:07:43 +0200

    Refactor to a newPerson state instead of individual states for number and name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,31 +4,29 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '079 549 44 18' }
   ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+
+  const [newPerson, setNewPerson] = useState({name: '', number: ''})
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
+    setNewPerson({...newPerson, name: event.target.value})
   }
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
+    setNewPerson({...newPerson, number: event.target.value})
   }
 
   const addPerson = (event) => {
     event.preventDefault()
     if (checkIfPersonExists()) {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newPerson.name} is already added to phonebook`)
       return
     }
-    const newPerson = {name: newName, number: newNumber}
     setPersons([...persons, newPerson])
-    setNewName('')
-    setNewNumber('')
+    setNewPerson({name: '', number: ''})
   }
 
   const checkIfPersonExists = () => {
-    return persons.some(person => person.name === newName)
+    return persons.some(person => person.name === newPerson.name)
   }
 
   return (
@@ -36,8 +34,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          <div>name: <input value={newName} onChange={handleNameChange} /></div>
-          <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+          <div>name: <input value={newPerson.name} onChange={handleNameChange} /></div>
+          <div>number: <input value={newPerson.number} onChange={handleNumberChange} /></div>
         </div>
         <div>
           <button type="submit">add</button>
