febf80ae81aa913ca07f363199ec2f7b216d11fc
Vanessa Sutter <vanessa@sutter.cloud>
2024-11-01 16:20:04 +0100

    Part 3 - Node.js and Express including fetching a single resource

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -20,11 +20,19 @@ let notes = [
 ]
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
-  })
+})
   
-  app.get('/api/notes', (request, response) => {
-    response.json(notes)
-  })
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  if (note) response.json(note)
+  response.statusMessage = `Note with id ${id} not found`
+  response.status(404).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
