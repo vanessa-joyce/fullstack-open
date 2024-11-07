b24808e34e434421d32ef104d022abfe40292129
Vanessa Sutter <vanessa@sutter.cloud>
2024-11-07 10:42:19 +0100

    Cleanup folders and make 3.9 Phonebook backend step 9 work

--- phonebook-frontend/src/App.jsx
+++ phonebook-frontend/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 
   useEffect(() => {
     axios
-    .get('http://localhost:3001/persons')
+    .get('http://localhost:3001/api/persons')
     .then(response => {
       setPersons(response.data)
     })
