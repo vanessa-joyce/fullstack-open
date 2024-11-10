39096cc0e2d018377f5ca330b88dc7c230f44add
Vanessa Sutter <vanessa@sutter.cloud>
2024-11-10 12:49:54 +0100

    Part 3 - Using database in route handlers

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -24,30 +24,26 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if (note) response.json(note)
-  response.statusMessage = `Note with id ${id} not found`
-  response.status(404).end()
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+    important: body.important || false,
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.put('/api/notes/:id', (request, response) => {
