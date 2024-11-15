36ab32c01f791b5dee6eba92b9396ab61a0ebfb7
Vanessa Sutter <vanessa@sutter.cloud>
2024-11-15 22:11:59 +0100

    Saving data to MongoDB w/o exercises 3.15 - 3.18

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const Note = require('./models/note')
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 const generateId = () => {
   const maxId = notes.length > 0
@@ -24,9 +24,15 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -46,23 +52,41 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.put('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const updatedNote = request.body
-  //const note = notes.find(note => note.id === id)
-  notes = notes.map((note) => 
-    note.id === id ? {...note, ...updatedNote} : note
-  )
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
-  response.json(updatedNote)
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
