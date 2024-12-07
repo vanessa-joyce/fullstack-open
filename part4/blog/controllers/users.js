5f0ccaaeed7c86b3365672641fab60ca977ccefc
Vanessa Sutter <vanessa@sutter.cloud>
2024-12-07 21:54:37 +0100

    4.15: Blog List Expansion, step 3

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.status(200).json(users)
+})
+
 usersRouter.post('/',async (request, response) => {
   const { username, name, password } = request.body
 
