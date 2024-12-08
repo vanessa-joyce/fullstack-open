b5fa3583cb38e495629b906d68482426e824161a
Vanessa Sutter <vanessa@sutter.cloud>
2024-12-08 17:49:55 +0100

    4.17: Blog List Expansion, step 5

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,13 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('createdBy')
   return response.status(200).json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const user = await User.findOne({})
+  console.log(user, 'user')
+  const blog = new Blog({ ...request.body, createdBy: user._id })
+
+  console.log(blog, 'blog')
 
   const result = await blog.save()
   return response.status(201).json(result)
