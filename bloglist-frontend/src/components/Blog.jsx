4c041bc4626ee1a9dac6eac2a13812d66834dd90
Vanessa Sutter <vanessa@sutter.cloud>
2024-12-16 21:24:09 +0100

    5.7 Blog List Frontend, step 7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,23 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(false)
+
+  return (
+    <div className="p-2 border border-solid border-indigo-600">
+      <div>
+        {blog.title} - {blog.author}
+      {showDetail === true ? (
+        <>
+        <button className="ml-3 py-2 px-3 text-sm rounded bg-purple-300 text-white" onClick={() => setShowDetail(false)}>hide</button>
+        <div>{blog.url}<br />likes {blog.likes}<br />{blog.createdBy?.name}</div>
+        </>
+      ) : 
+      <button className="ml-3 py-2 px-3 text-sm rounded bg-purple-300 text-white" onClick={() => setShowDetail(true)}>show</button>
+      }
+      </div>
+    </div> 
+  ) 
+}
 
 export default Blog
